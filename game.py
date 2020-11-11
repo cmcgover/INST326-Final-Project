a5dc2558f2f0f9a9108bcf2f261dac63fd9996a2
amanu huq <amanuhuq@amanus-MBP.fios-router.home>
2020-11-10 21:38:48 -0500

    def genre added

---
+++ game.py
@@ -0,0 +1,8 @@
+class MadlibsTemplate:
+    def genre(self, genre):
+        """ Choose the genre of the story to be generated
+        Args:
+        genre(str) User chooses between different genres such as comedy, horror etc.
+        Side effects:
+        Generates a story of the genre choosen
+        """
