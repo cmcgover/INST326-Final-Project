41efa9d85a85bda09df0bee6df93a89cd28f73c8
Chelsea McGovern <cgmcgovern1@gmail.com>
2020-11-10 21:58:41 -0500

    generator method created

--- madlibs_temp.py
+++ madlibs_temp.py
@@ -14,3 +14,23 @@ class Template:
         """
         genre = input("What type of story would you like? comedy/horror/romance/celebrity ")
         
+    def format(self): #Casey
+        """ Creating the format of the Mad Libs template regarding word count, # of type of words, etc.
+        
+        Args:
+        num_nouns(int): Total # of nouns in story
+        num_verbs(int): Total # of verbs in story
+        num_adverbs(int): Total # of adverbs in story
+        num_adjectives(int): Total # of adjectives in story
+        
+        Side effects:
+        # of words for each word type in a dictionary
+        """
+        
+    def generator(self, story):
+        """Compiles story into a Mad Libs template text file
+        Arg:
+        story(dict): total words in mad libs template
+        Side effects:
+        file(str): text file 
+        """
