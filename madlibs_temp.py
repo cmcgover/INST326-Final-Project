61e6da51b2af35b5da633a2ee48f4d433ac7bf87
Alhaji Bah <alhaji@Alhajis-MBP-2.fios-router.home>
2020-11-10 21:38:05 -0500

    added genre method

--- madlibs_temp.py
+++ madlibs_temp.py
@@ -1,4 +1,12 @@
 class Template:
-    '''Generates the template
+    """Generates the template
     
-    '''
+    """
+    def genre(self, genre):
+        """ Choose the genre of the story to be generated
+        Args:
+        genre(str) User chooses between different genres such as comedy, horror etc.
+        Side effects:
+        Generates a story of the genre choosen
+        """
+    
