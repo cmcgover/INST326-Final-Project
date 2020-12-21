42816d3b6bea42d67087071a47a21af85712b909
amanuhuq <73295694+amanuhuq@users.noreply.github.com>
2020-12-21 18:35:43 -0500

    test script

--- test_madlib.py
+++ test_madlib.py
@@ -1,7 +1,11 @@
 #Test script for madlibs_temp
-
+from madlibs_temps import genre, user_choice,read, generator, format
+ 
 def test_genre():
-    
+    assert self.content
+    assert self.d.keys() in ['park'] 
+    assert len(self.d.keys()) ==  1
+        
 def test_user_choice():
     
 def test_read():
